56a192f14baff1c3d2dcd0a6ad483016cd8115a0
vishal tiwari <vishaltiwari7086@gmail.com>
2026-05-23 18:25:26 +0530

    Fixed Railway host settings

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 8080
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: true
+  }
 })
